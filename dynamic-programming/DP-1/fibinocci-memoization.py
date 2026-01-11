6120c1476c14dbf0850332296e66109c8c9f5ad0
Saagar <vasamsetty@gmail.com>
2026-01-11 11:44:13 -0500

    fibinocci

---
+++ dynamic-programming/DP-1/fibinocci-memoization.py
@@ -0,0 +1,27 @@
+
+class Fibinocci:
+    def __init__(self):
+        self.memo = {}
+    def fibinocci_recursive(self, n:int):
+        if n==0 or n==1:
+            return n
+        
+        return self.fibinocci_recursive(n-1) + self.fibinocci_recursive(n-2)
+    
+    def fibinocci_memoization(self, n:int):
+        if n in self.memo:
+            return self.memo[n]
+        
+        if n==0 or n==1:
+            return n
+        else:
+            self.memo[n] = self.fibinocci_memoization(n - 1) + self.fibinocci_memoization(n-2)
+        
+        return self.memo[n]
+    
+
+if __name__ == "__main__":  
+    f = Fibinocci()
+    print(f.fibinocci_memoization(8))
+    
+    print(f.fibinocci_recursive(8))
