7023b1d41bb615d37bdbbf4a2d34bb8f11b683e0
Saagar <vasamsetty@gmail.com>
2026-01-11 19:13:55 -0500

    PDFs

--- dynamic-programming/DP-1/1137-tribonacci.py
+++ dynamic-programming/DP-1/1137-tribonacci.py
@@ -1,7 +1,7 @@
 
 class Tribonacci:
-    
-    def tribonacci(self, n:int):
+    # Time: O(n) and Space: O(n)
+    def tribonacci_v1(self, n:int):
         dp = [0]*(n+1)
         
         dp[0] = 0
@@ -12,8 +12,21 @@ class Tribonacci:
             dp[i] = dp[i-1]+ dp[i-2]+dp[i-3]
         
         return dp[n]
+    
+    # Time: O(n) and Space: O(1)
+    def tribonacci_v2(self, n:int):
+        var0 = 0
+        var1 = 1
+        var2 = 1
+        
+        for _ in range(3, n+1):
+            curr = var0 + var1 + var2
+            var0, var1, var2 = var1, var2, curr
+        
+        return curr
 
 if __name__ == "__main__": 
     dp_sol = Tribonacci()
-    print(dp_sol.tribonacci(25))
+    print(dp_sol.tribonacci_v1(25))
+    print(dp_sol.tribonacci_v2(25))
         
